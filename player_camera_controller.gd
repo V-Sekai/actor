14da6f2594067ebe49ec8e4ec711063561808772
Saracen <SaracenOne@gmail.com>
2021-02-11 12:39:08 +0000

    Add work in progress camera collision

--- player_camera_controller.gd
+++ player_camera_controller.gd
@@ -7,10 +7,14 @@ var origin: Spatial = null
 var camera: Camera = null
 
 export (bool) var is_active: bool = true
+export (int, LAYERS_3D_PHYSICS) var camera_clip_layers: int = 1
 
 # Camera mode
 enum { CAMERA_FIRST_PERSON, CAMERA_THIRD_PERSON }
 var camera_mode: int = CAMERA_FIRST_PERSON
+
+# Side
+var distance: float = 4.0
 
 # Side
 var side_offset: float = 0.25
@@ -30,6 +34,36 @@ var rotation_pitch_max: float = deg2rad(89.5)
 
 var origin_offset: Vector3 = Vector3()
 
+func test_collision_point(p_ds: PhysicsDirectSpaceState, p_distance: float, p_start: Vector3, p_end: Vector3) -> float:
+	var result = p_ds.intersect_ray(p_start, p_end, [get_parent().global_transform.origin], camera_clip_layers, true, false)
+	if(result.empty() == false):
+		var new_distance = p_start.distance_to(result.position)
+		if(new_distance < p_distance):
+			return new_distance
+			
+	return p_distance
+
+func translate_third_person_camera(p_distance: float, p_corrected_pitch: float) -> void:
+	translate(Vector3(1.0, 0.0, 0.0) * side_offset)
+	translate(Vector3(0.0, 0.0, 1.0) * p_distance * cos(p_corrected_pitch))
+	translate(Vector3(0.0, 1.0, 0.0) * p_distance * sin(p_corrected_pitch))
+
+func get_camera_clip_distance(p_camera) -> float:
+	var ds: PhysicsDirectSpaceState = PhysicsServer.space_get_direct_state(get_world().get_space())
+	
+	var collision_distance = distance
+	var start_transform: Transform = get_parent().get_global_transform()
+	var end_transform = get_global_transform()
+	
+	var upper_left = end_transform.origin - p_camera.project_position(Vector2(0.0, 0.0), 0.0)
+	var upper_right = end_transform.origin - p_camera.project_position(Vector2(OS.get_window_size().x, 0.0), 0.0)
+	var bottom_left = end_transform.origin - p_camera.project_position(Vector2(0.0, OS.get_window_size().y), 0.0)
+	var bottom_right = end_transform.origin - p_camera.project_position(Vector2(OS.get_window_size().x, OS.get_window_size().y), 0.0)
+	
+	collision_distance = test_collision_point(ds, collision_distance, start_transform.origin, end_transform.origin)
+	
+	return collision_distance
+
 func update(p_delta: float) -> void:
 	var corrected_pitch: float = 0.0
 	if is_active and ! VRManager.is_xr_active():
@@ -46,9 +80,12 @@ func update(p_delta: float) -> void:
 		
 	# Third-person camera
 	if camera_mode == CAMERA_THIRD_PERSON:
-		translate(Vector3(1.0, 0.0, 0.0) * side_offset)
-		translate(Vector3(0.0, 0.0, 1.0) * cos(corrected_pitch))
-		translate(Vector3(0.0, 1.0, 0.0) * sin(corrected_pitch))
+		translate_third_person_camera(distance, corrected_pitch)
+	
+		var new_distance: float = get_camera_clip_distance(camera)
+		transform.origin = Vector3()
+		
+		translate_third_person_camera(new_distance, corrected_pitch)
 	else:
 		transform.origin = Vector3()
 		

09c43d7642cce63f47efede4b39dfcc4534c7e01
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-24 19:49:34 -0700

    Restore old code.

--- movement_controller.gd
+++ movement_controller.gd
@@ -21,6 +21,13 @@ func set_global_origin(p_origin: Vector3, _p_update_physics: bool = false) -> vo
 			_character_body.set_global_transform(Transform3D(Basis(), get_global_origin()))
 
 
+func set_global_transform(p_global_transform: Transform3D, _p_update_physics: bool = false) -> void:
+	super.set_global_transform(p_global_transform, _p_update_physics)
+	if _p_update_physics:
+		if _character_body and _character_body.is_inside_tree():
+			_character_body.set_global_transform(Transform3D(Basis(), get_global_origin()))
+
+
 func set_character_body(p_character_body: CharacterBody3D) -> void:
 	_character_body = p_character_body
 

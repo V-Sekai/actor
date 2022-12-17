eba918891c1117e42a0ba1f567221b16afc06eed
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-12-16 23:59:53 -0800

    Remove dead code and comments that repeat variable names.

--- actor_controller.gd
+++ actor_controller.gd
@@ -6,6 +6,7 @@ const state_machine_const = preload("res://addons/actor/actor_state_machine.gd")
 @export var _state_machine_path: NodePath = NodePath()
 var _state_machine: Node = null # state_machine_const
 
+
 # Render
 @export var _third_person_render_node_path: NodePath = NodePath()
 var _third_person_render_node: Node = null
@@ -16,7 +17,6 @@ var velocity: Vector3 = Vector3() :
 	get = get_velocity
 
 
-
 func set_velocity(p_velocity: Vector3) -> void:
 	velocity = p_velocity
 
@@ -25,9 +25,6 @@ func get_velocity() -> Vector3:
 	return velocity
 
 
-#
-
-# Movement stats
 @export var sprint_speed: float = 10.0:
 	set = set_sprint_speed,
 	get = get_sprint_speed
@@ -39,7 +36,6 @@ func get_velocity() -> Vector3:
 @export var fly_speed: float = 10.0:
 	set = set_fly_speed,
 	get = get_fly_speed
-
 
 
 func set_sprint_speed(p_speed: float) -> void:
@@ -65,22 +61,16 @@ func set_fly_speed(p_speed: float) -> void:
 func get_fly_speed() -> float:
 	return fly_speed
 
-# Render
 @export var _render_node_path: NodePath = NodePath()
 var _render_node: Node3D = null
-
-#var skeleton = null : Spatial
-
 
 func cache_nodes() -> void:
 	super.cache_nodes()
 	
-	# Render node
 	_render_node = get_node_or_null(_render_node_path)
 	if _render_node == self or not _render_node is Node3D:
 		_render_node = null
 
-	# State machine node
 	_state_machine = get_node_or_null(_state_machine_path)
 	if _state_machine == self:
 		_state_machine = null
